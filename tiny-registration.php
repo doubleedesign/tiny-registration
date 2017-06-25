deaa30628214f6c480f0cb44816c5cb2373bd035
Leesa Ward <leesa@doubleedesign.com.au>
2017-06-25 17:16:57 +1000

    Updated URL to my GitHub repo URL

--- tiny-registration.php
+++ tiny-registration.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 Plugin Name: Tiny Registration
-Plugin URI: http://wp.tribuna.lt/tiny-login
+Plugin URI: https://github.com/doubleedesign/tiny-registration
 Description: A simple front-end registration form for WordPress, forked from tinyLogin (http://wp.tribuna.lt/tiny-login). Adds template tags and shortcodes. Shortcodes: [tiny_form_register]. Template tags: get_tiny_form_register() and the_tiny_form_register(). Original code URL: https://gist.github.com/ideag/5582347#file-tiny_login-php
 Version: 0.1
 Author: Leesa Ward / Double-E Design
